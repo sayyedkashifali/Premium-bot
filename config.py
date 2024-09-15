8dcc2cd5a0252df558acb76598d8ff6c4632ff86
sayyedkashifali <sayyedkashifali586@gmail.com>
2024-09-15 18:58:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
-API_ID =os.getenv("27317700")
-API_HASH =os.getenv("de1077f45e29e6abebcd2b9dd196be1d")
-BOT_TOKEN =os.getenv("7335984411:AAGwpqyCseguoIBo5wlW-Uqzos3NuCUiLcQ")
+
+API_ID = os.getenv("27317700")
+API_HASH = os.getenv("de1077f45e29e6abebcd2b9dd196be1d")
+BOT_TOKEN = os.getenv("7335984411:AAGwpqyCseguoIBo5wlW-Uqzos3NuCUiLcQ")
