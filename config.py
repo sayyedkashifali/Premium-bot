4e95a1954726cb1b30aff0f4567ae67d5ad2c67d
sayyedkashifali <sayyedkashifali586@gmail.com>
2024-09-15 18:44:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
-API_ID = "27317700"
-API_HASH = "de1077f45e29e6abebcd2b9dd196be1d"
-BOT_TOKEN = "7335984411:AAGwpqyCseguoIBo5wlW-Uqzos3NuCUiLcQ"
+import os
+API_ID =os.getenv("27317700")
+API_HASH =os.getenv("de1077f45e29e6abebcd2b9dd196be1d")
+BOT_TOKEN =os.getenv("7335984411:AAGwpqyCseguoIBo5wlW-Uqzos3NuCUiLcQ")
